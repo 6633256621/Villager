d6210bcdc9bfa1e16099a65703801667b80d595f
6633256621 <6633256621@student.chula.ac.th>
2024-02-22 00:47:38 +0700

    perfect walk

--- src/ent/Player.java
+++ src/ent/Player.java
@@ -2,29 +2,95 @@ package ent;
 
 import input.InputUtility;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import panel.Gamepanel;
+import javafx.scene.image.Image;
 
-public class Player extends Entity{
-    private static final int speed =1;
+import java.awt.*;
+
+public class Player extends Entity {
+    private static final int speed = 2;
     private int size = Gamepanel.getInstance().getTileSize();
+    private int spriteCounter = 0;
+    private int spriteNum = 1;
+    private Image down = new Image(ClassLoader.getSystemResourceAsStream("Character_Down.png"));
+    private Image up = new Image(ClassLoader.getSystemResourceAsStream("up.png"));
+
+    private Image left = new Image(ClassLoader.getSystemResourceAsStream("left.png"));
+
+    private Image right = new Image(ClassLoader.getSystemResourceAsStream("right.png"));
+    private Image down2 = new Image(ClassLoader.getSystemResourceAsStream("down2.png"));
+    private Image down3 = new Image(ClassLoader.getSystemResourceAsStream("down3.png"));
+    private Image up2 = new Image(ClassLoader.getSystemResourceAsStream("up2.png"));
+    private Image up3 = new Image(ClassLoader.getSystemResourceAsStream("up3.png"));
+    private Image left2 = new Image(ClassLoader.getSystemResourceAsStream("left2.png"));
+    private Image left3 = new Image(ClassLoader.getSystemResourceAsStream("left3.png"));
+    private Image right2 = new Image(ClassLoader.getSystemResourceAsStream("right2.png"));
+    private Image right3 = new Image(ClassLoader.getSystemResourceAsStream("right3.png"));
+
+    private Image def;
+
     public Player() {
-        x=100;
-        y=100;
+        x = 100;
+        y = 100;
+        z=2;
+        def = down;
     }
+
     private void up() {
-        y-=speed;
+        y -= speed;
+        if (spriteNum == 1) {
+            def = up;
+        }
+        if (spriteNum == 2) {
+            def = up2;
+        }
+        if (spriteNum == 3) {
+            def = up3;
+        }
     }
+
     private void down() {
-        y+=speed;
+        y += speed;
+        if (spriteNum == 1) {
+            def = down;
+        }
+        if (spriteNum == 2) {
+            def = down2;
+        }
+        if (spriteNum == 3) {
+            def = down3;
+        }
     }
+
     private void left() {
-        x-=speed;
+        x -= speed;
+        if (spriteNum == 1) {
+            def = left;
+        }
+        if (spriteNum == 2) {
+            def = left2;
+        }
+        if (spriteNum == 3) {
+            def = left3;
+        }
     }
+
     private void right() {
-        x+=speed;
+        x += speed;
+        if (spriteNum == 1) {
+            def = right;
+        }
+        if (spriteNum == 2) {
+            def = right2;
+        }
+        if (spriteNum == 3) {
+            def = right3;
+        }
     }
+
     public void update() {
         if (InputUtility.getKeyPressed(KeyCode.W)) {
             up();
@@ -38,12 +104,25 @@ public class Player extends Entity{
         if (InputUtility.getKeyPressed(KeyCode.D)) {
             right();
         }
+        spriteCounter++;
+        if (spriteCounter > 60) {
+            spriteCounter = 0;
+        } else {
+            if (spriteCounter < 20) {
+                spriteNum = 1;
+            } else if (spriteCounter > 20 && spriteCounter <= 40) {
+                spriteNum = 2;
+            } else if (spriteCounter > 40) {
+                spriteNum = 3;
+            }
+        }
     }
 
     @Override
     public void draw(GraphicsContext gc) {
-        gc.clearRect(0, 0, Gamepanel.getInstance().getWidth(), Gamepanel.getInstance().getHeight());
-        gc.setFill(Color.BLACK);
-        gc.fillRect(x,y,size,size);
+        gc.clearRect(0, 0, Gamepanel.getInstance().getScreenWidth(), Gamepanel.getInstance().getScreenHeight());
+        gc.drawImage(def, x, y, size, size);
     }
+
+
 }
