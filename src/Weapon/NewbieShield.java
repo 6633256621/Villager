afc3bea4b36aef9bb6c335d8ad62f2d94a642fef
6633256621 <6633256621@student.chula.ac.th>
2024-02-23 14:26:49 +0700

    first background

--- src/Weapon/NewbieShield.java
+++ src/Weapon/NewbieShield.java
@@ -5,6 +5,6 @@ import javafx.scene.image.Image;
 public class NewbieShield extends BaseShield {
     public NewbieShield() {
         setDefenseValue(1);
-        image = new Image(ClassLoader.getSystemResourceAsStream("newbieShield.png"));
+        image = new Image(ClassLoader.getSystemResourceAsStream("shield/newbieShield.png"));
     }
 }
--- src/panel/GamePanel.java
+++ src/panel/GamePanel.java
@@ -6,6 +6,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import render.Renderable;
 import render.RenderableHolder;
+import tile.TileManager;
 import utility.InputUtility;
 
 public class GamePanel extends Canvas {
@@ -18,6 +19,7 @@ public class GamePanel extends Canvas {
     private int maxRowScreen = 12;//row ratio
     private int screenWidth = maxColScreen * tileSize;//768
     private int screenHeight = maxRowScreen * tileSize;//576
+    private TileManager tileManager = new TileManager(this);
     private GraphicsContext gc = this.getGraphicsContext2D();
     public UserInterface ui = new UserInterface(this);
 
@@ -32,6 +34,7 @@ public class GamePanel extends Canvas {
     //draw all entity
     public void paintComponent() {
         gc.setFill(Color.BLACK);
+        tileManager.draw(gc);
         for (Renderable entity : RenderableHolder.getInstance().getEntities()) {
             if (entity.isVisible()) {
                 entity.draw(gc);//draw each entity
@@ -64,5 +67,13 @@ public class GamePanel extends Canvas {
 
     public GraphicsContext getGc() {
         return gc;
+    }
+
+    public int getMaxColScreen() {
+        return maxColScreen;
+    }
+
+    public int getMaxRowScreen() {
+        return maxRowScreen;
     }
 }
