83367bacad0a590e231b62fcfffe8e4b8ae666f5
6633256621 <6633256621@student.chula.ac.th>
2024-03-15 10:41:23 +0700

    inventory 1 (without paimee)

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,36 +10,38 @@ import javafx.stage.Stage;
 import panel.GamePanel;
 import panel.RootPane;
 import render.RenderableHolder;
+import utility.UserInterface;
 
 
 public class Main extends Application {
     public static void Main(String[] args) {
         launch(args);
     }
+    public static int count=0;
 
     @Override
     public void start(Stage stage) throws Exception {
         //setup pane
         RootPane rootpane = new RootPane();
-        rootpane.addlistener();
         GamePanel gamepanel = GamePanel.getInstance();
         GameLogic logic = new GameLogic();
         RenderableHolder renderableHolder = RenderableHolder.getInstance();
-        GraphicsContext gc = gamepanel.getGraphicsContext2D();
-        rootpane.getChildren().addAll(gamepanel);
+        rootpane.getChildren().add(gamepanel);
+        gamepanel.setFocusTraversable(true);
+
         //setup scene
-        Scene scene = new Scene(rootpane);
+        Scene scene = new Scene(rootpane,Config.screenWidth,Config.screenHeight);
         //setup stage
         stage.setScene(scene);
         stage.setTitle("Villager");
         stage.setResizable(false);
         stage.show();
-        scene.getRoot().requestFocus();
 
         //animation timer for drawing
         AnimationTimer animation = new AnimationTimer() {
             @Override
             public void handle(long l) {
+
                 gamepanel.paintComponent();
                 logic.logicUpdate();
                 renderableHolder.update();
